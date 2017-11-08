5f6f66ffa6679a370648271cc19b52d68e335ecc
amiyatomoki <tomoki9-29@ezweb.ne.jp>
2017-11-08 06:44:34 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', 'WelcomeController@index');
 
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
@@ -26,4 +27,5 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 //ログイン認証付きのルーティング(index,show)
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
